1023275741d639c4d47e2a7e8a707e7c997bbc13
Uimin Maksim <uimin1maksim@yandex.ru>
2019-09-22 13:10:35 +0300

    Header parsing loop exit predicate changed

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,8 @@ FROM ubuntu:latest
 WORKDIR /opt/httpd
 COPY . .
 RUN apt-get update && yes | \
-    apt-get install "libevent-dev" "zlib1g-dev" "cmake"
+    apt-get install "libevent-dev" "zlib1g-dev" "cmake" \
+    "vim" #TODO remove
 RUN ./main.sh
 EXPOSE 80
 #ENTRYPOINT ["./bin/HighloadServer", "/etc/httpd.conf", "2>&1"]
--- src/http.c
+++ src/http.c
@@ -81,17 +81,17 @@ static void parse_http_req_headers(char** req_str, struct http_request_t* req) {
     size_t header_len = 0;
 
     while(1) {
+        if (*cursor[0] == '\r' && *cursor[1] == '\n') {
+            *cursor += delim_len;
+            break;
+        }
         struct http_header_t header = HTTP_HEADER_INITIALIZER;
         *header_ptr = &header;
         (*header_ptr)->text = *cursor;
         prev_cursor_val = *cursor;
         *cursor = strstr(*cursor, delim);
-        header_len = prev_cursor_val - *cursor;
+        header_len = *cursor - prev_cursor_val;
         log(DEBUG, "header_len: %d", header_len);
-        if(header_len == delim_len) {
-            *cursor += delim_len;
-            break;
-        }
         if (*cursor == NULL) {
             log(ERROR, "Can't parse headers: empty line does not reached");
             req->headers = NULL;
